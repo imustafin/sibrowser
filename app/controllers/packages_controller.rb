546389627ac8fe65dd6b31810767d4dbbf9fe016
Ilgiz Mustafin <ilgimustafin@gmail.com>
2021-05-19 22:02:55 +0300

    Generate packages controller

---
+++ app/controllers/packages_controller.rb
@@ -0,0 +1,4 @@
+class PackagesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'packages#index'
+
+  get 'packages', to: 'packages#index'
 end
