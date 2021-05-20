d16b824f8a0a1a44f5684b8d17e3c3bb4b8afa25
Ilgiz Mustafin <ilgimustafin@gmail.com>
2021-05-21 00:46:58 +0300

    Add basic styling

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,5 +1,6 @@
 class PackagesController < ApplicationController
+
   def index
-    @packages = Package.order(:id).page(params[:page])
+    @packages = Package.order(:id).page(params[:page]).per(10)
   end
 end
