af7d84e26ade5841cb59e5e30aa68ce140a02cb8
Ilgiz Mustafin <ilgimustafin@gmail.com>
2021-05-24 02:29:02 +0300

    Purge tailwind

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: {
+    // from https://github.com/rails/tailwindcss-rails/pull/40/commits/6cdb6cd5d2d07f1affa08fd9d57720d3cd00416d
+    enabled: ["production"].includes(process.env.NODE_ENV),
+    content: [
+      './app/views/**/*.*',
+      './app/helpers/**/*.rb',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
