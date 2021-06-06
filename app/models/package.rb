830925edebf7ea47a73fa5b91233496f1561a40d
Ilgiz Mustafin <ilgimustafin@gmail.com>
2021-06-06 19:37:28 +0300

    Don't skip updating packages without structure

--- app/models/package.rb
+++ app/models/package.rb
@@ -55,8 +55,8 @@ class Package < ApplicationRecord
       unless model
         create!(params)
       else
-        # Same login as in .skip_updating?
-        if params[:published_at] < model.published_at || model.version < VERSION
+        # Same logic as in .skip_updating?
+        if params[:published_at] < model.published_at || model.version < VERSION || model.structure.blank?
           model.update(params)
           model.save!
         end
@@ -70,6 +70,7 @@ class Package < ApplicationRecord
     where(vk_document_id: new_vk_document_id)
       .where('published_at <= ?', new_published_at) # The older post can have a more relevant original_text
       .where('version >= ?', VERSION) # same version is compatible, greater version should not happen
+      .where('structure IS NOT NULL') # parse if structure was deleted when upgrading
       .exists?
   end
 end
