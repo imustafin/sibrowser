6266860a00719bc8e7fe01281f81a08845dda32d
Ilgiz Mustafin <ilgimustafin@gmail.com>
2021-08-01 23:51:47 +0300

    Fix author show author link

--- app/components/author_link_component.rb
+++ app/components/author_link_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class AuthorLinkComponent < ViewComponent::Base
-  def initialize(author:)
+  def initialize(author:, author_itemprop: true)
     @author = author
+    @author_itemprop = author_itemprop
   end
 
 end
