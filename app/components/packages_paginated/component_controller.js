a2cfa8329f01ec8a5ce73be98518bc9339cec651
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-01-11 23:26:16 +0300

    Sticky pagination

--- app/components/packages_paginated/component_controller.js
+++ app/components/packages_paginated/component_controller.js
@@ -1,7 +1,42 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [
+    "pagination",
+    "afterLast",
+    "beforeFloating",
+  ]
+
   connect() {
-    console.log("connected", this.element);
+    this.createObserver();
+  }
+
+  createObserver() {
+    const observer = new IntersectionObserver(es => this.handleIntersect(es), {
+      threshold: [0, 1],
+    });
+
+    observer.observe(this.afterLastTarget);
+    observer.observe(this.beforeFloatingTarget);
+  }
+
+  handleIntersect(entries) {
+    entries.forEach(entry => {
+      if (entry.target === this.afterLastTarget) {
+        this.afterLastIntersects(entry);
+      }
+      if (entry.target === this.beforeFloatingTarget) {
+        this.beforeFloatingIntersects(entry);
+      }
+    });
+  }
+
+  afterLastIntersects(intersection) {
+
+  }
+
+  beforeFloatingIntersects(intersection) {
+    const inter = intersection.isIntersecting;
+    this.paginationTarget.classList.toggle('bg-white', !inter);
   }
 }
