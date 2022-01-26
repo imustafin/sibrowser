dd3f65efe5862c2330bf3ead029ff4d1a737c5c6
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-01-26 23:22:25 +0300

    Cache static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
