0be6e8ed913d333589f2dab1a089b96e210e4f1b
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-02-02 22:38:21 +0300

    Use infinite pagination for categories#show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
+  include PackagesPagination
+
+  helper_method :category, :packages
+
   def index
     @categories = SibrowserConfig::CATEGORIES
       .map { |c| [c, Package.by_category(c).count] }
@@ -9,17 +13,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = params[:id]
+    return packages_pagination(packages) if request.format.turbo_stream?
 
-    @packages = Package
-      .visible_paged(params[:page])
-      .by_category(@category)
-      .reorder_by_category(@category)
+    package_count = packages.total_count
 
-    @package_count = @packages.total_count
-
-    @page_title = t('title_category', category: @category)
-    @page_description = t('description_category', category: @category, package_count: @package_count)
+    @page_title = t('title_category', category:)
+    @page_description = t('description_category', category:, package_count:)
 
     set_meta_tags noindex: params['page'].present?
 
@@ -29,6 +28,17 @@ class CategoriesController < ApplicationController
         @category
       ]
     }
+  end
+
+  def category
+    params[:id]
+  end
+
+  def packages
+    Package
+      .visible_paged(params[:page])
+      .by_category(category)
+      .reorder_by_category(category)
   end
 
 end
