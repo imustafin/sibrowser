a4a385a17a9fdd626f57d9aab037c98f42747edf
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-08-09 23:43:05 +0300

    Kinda working nbayes

--- app/features/classification/classifier.rb
+++ app/features/classification/classifier.rb
@@ -8,132 +8,118 @@ module Classification
     #   |d| = number of words (len(d))
 
     def initialize
-      tmp = 'create temporary table'
+      tmp = 'create temporary table if not exists'
       execute <<~SQL
         #{tmp}
-        lens (
-          package_id bigint,
-          len int
-        );
-
-        #{tmp}
-        tfs (
-          package_id bigint,
-          term text,
-          tf float
-        );
-
-        #{tmp}
-        idfs (
-          term text,
-          idf float
+        goodterms (
+          term text
         );
 
         #{tmp}
         pcategories (
-          id int,
           package_id bigint,
           category text
         );
 
         #{tmp}
         aprioris (
-          id int,
+          category text,
+          probability float
+        );
+
+        #{tmp}
+        termprobs (
+          term text,
+          category text,
+          probability float
+        );
+
+        #{tmp}
+        packprobs (
+          package_id bigint,
           category text,
           probability float
         )
       SQL
 
-      model(:len) do
-        belongs_to :package
-      end
-
-      model(:tf) do
-        belongs_to :package
-      end
-
-      model(:idf)
+      model(:goodterm)
 
       model(:pcategory) do
         belongs_to :package
       end
 
       model(:apriori)
+
+      model(:termprob)
+
+      model(:packprob)
     end
 
     def prepare
-      report(Len) { fill_len }
-      report(Tf) { fill_tf }
-      report(Idf) { fill_idf }
+      report(Goodterm) { fill_goodterms }
       report(Pcategory) { fill_pcategory }
       report(Apriori) { fill_apriori }
+      report(Termprob) { fill_termprobs }
+      report(Packprob) { fill_packprobs }
 
       self
     end
 
     def report(model, &blk)
       puts "Filling #{model}"
-      res = Benchmark.measure(&blk)
-      puts "Filled #{model.count} of #{model} in #{res.total}"
+      start = Time.now
+      blk.call
+      finish = Time.now
+      puts "Filled #{model.count} of #{model} in #{(finish - start).round(2)}"
     end
 
+    def fill_goodterms
+      all_terms = Package
+        .from(Package.select(unnest_ts))
+        .select('(ts).lexeme as term')
+        .distinct
 
-    # len(d) is length of document d
-    def fill_len
-      lens = Package
-        .from(Package.select(:id, unnest_ts))
-        .select(:id, 'sum(array_length((ts).positions, 1)) as len')
-        .group(:id)
+      puts "Initial distinct terms #{Package.from(all_terms).count}"
 
-      insert(Len, lens)
-    end
+      good_terms = Package
+        .from(all_terms)
+        .select('term')
+        .where('char_length(term) > 2 or char_length(term) < 10')
+        .where("term similar to '[a-zа-я]*'")
 
-    def fill_tf
-      data = Package
-        .from(Package.select(:id, unnest_ts))
-        .joins("JOIN #{Len.table_name} ON id = package_id")
-        .select(
-          :id,
-          '(ts).lexeme',
-          '(array_length((ts).positions, 1)::float / len)'
-        )
+      puts "Cleaned terms #{Package.from(good_terms).count}"
 
-      insert(Tf, data)
-    end
-
-    def fill_idf
-      n = Package.count # N = number of documents
-
-      data = Package
-        .from(Package.select(:id, unnest_ts))
-        .group('(ts).lexeme')
-        .select(
-          '(ts).lexeme',
-          "log(#{n}::float / count(*)::float)"
-        )
-
-      insert(Idf, data)
+      # insert(Goodterm, good_terms)
+      insert(Goodterm, all_terms)
     end
 
     def fill_pcategory
+      inserts = []
       Package.where.not(tags: []).select(:id, :tags).find_in_batches do |batch|
-        inserts = []
-
         batch.map do |p|
           next unless p.tags
 
           cats = p.tags.map { |t| category_by_tag(t) }.compact
 
-          cats.compact.each do |cat|
-            inserts << {
-              package_id: p.id,
-              category: cat
-            }
+          if cats.include?('anime')
+            inserts << [p.id, 'anime']
+          else
+            inserts << [p.id, 'not_anime']
           end
-        end
 
-        Pcategory.create!(inserts)
+
+          # cats.each do |cat|
+          #   inserts << [p.id, cat]
+          # end
+
+          # (CAT_TO_TAG.keys - cats).each do |cat|
+          #   inserts << [p.id, "not_#{cat}"]
+          # end
+        end
       end
+
+      irows = inserts.map { |(a, b)| "(#{a}, '#{b}')" }.join(', ')
+      insert(Pcategory, "values #{irows}")
     end
 
     def category_by_tag(tag)
@@ -141,19 +127,111 @@ module Classification
     end
 
     def fill_apriori
-      distinct_ids = Pcategory.select(:package_id).distinct.count
+      distinct_ids = Pcategory.select(:package_id).count
+      inserts = []
 
-      CAT_TO_TAG.keys.each do |category|
+      all_cats = Pcategory.distinct.pluck(:category)
+
+      all_cats.each do |category|
         this_cat = Pcategory.where(category:).count.to_f
         prob =  this_cat / distinct_ids
-        Apriori.create!(
-          category:,
-          probability: prob
-        )
+
+        inserts << [category, prob]
       end
+
+      rows = inserts.map { |(a, b)| "('#{a}', #{b})" }.join(', ')
+
+      insert(Apriori, "values #{rows}")
     end
 
+    def fill_termprobs
+      all_terms = Goodterm.all
+
+      all_terms_n = Goodterm.count
+
+      all_categories = Pcategory.select('category').distinct
+
+      term_cat = Package
+        .from(all_terms)
+        .select('term', 'category')
+        .joins("CROSS JOIN (#{all_categories.to_sql}) s")
+
+      real_counts = Pcategory
+        .joins("JOIN (#{Package.select('id', unnest_ts).to_sql}) s ON package_id = s.id")
+        .joins("JOIN #{Goodterm.table_name} on term = (ts).lexeme")
+        .group('category', '(ts).lexeme')
+        .select(
+          '(ts).lexeme AS term',
+          'category',
+          "sum(#{ts_occurs}) as real_count"
+        )
+
+      category_len = Package
+        .from(real_counts)
+        .select('category', 'sum(real_count) as category_len')
+        .group('category')
+
+      data = Package
+        .from(term_cat)
+        .select('term', 'category',
+          # 'real_count', 'category_len',
+          # Laplace smoothing here
+          "(coalesce(real_count, 0) + 1)::float / (category_len + #{all_terms_n})")
+        .joins("LEFT OUTER JOIN (#{real_counts.to_sql}) rr using (term, category)")
+        .joins("JOIN (#{category_len.to_sql}) clen using(category)")
+
+      insert(Termprob, data)
+    end
+
+    def fill_packprobs
+      package_term_counts = Package
+        .from(Package.select('id', unnest_ts))
+        .select('id', '(ts).lexeme as term', "(#{ts_occurs}) as occurs")
+        .joins("JOIN #{Goodterm.table_name} ON term = (ts).lexeme")
+
+      package_tc_probs = Package
+        .from(package_term_counts)
+        .select('id', 'term', 'category', 'occurs * log(probability) as term_log_prob')
+        .joins("JOIN #{Termprob.table_name} USING (term)")
+
+      package_terms_log_prob = Package
+        .from(package_tc_probs)
+        .select('id', 'category', 'sum(term_log_prob) as term_log_prob_sum')
+        .group('id', 'category')
+
+      with_apriori = Package
+        .from(package_terms_log_prob)
+        .select('id', 'category',
+          "(term_log_prob_sum + log(#{Apriori.table_name}.probability)) AS log_prob")
+        .joins("JOIN #{Apriori.table_name} using (category)")
+
+
+      pp Package.joins("JOIN (#{with_apriori.to_sql}) s USING (id)")
+        .select('id', 'name', 'tags', 's.category', 'log_prob', "#{Pcategory.table_name}.category as pcat")
+        .where("packages.tags <> '[]'")
+        .where("#{Pcategory.table_name}.category = 'anime'")
+         .where('id = 17004')
+        .joins("JOIN #{Pcategory.table_name} on id = package_id")
+        .limit(10)
+        # .order('log_prob desc')
+        .order(
+          # :id,
+          'log_prob desc'
+        )
+        .as_json
+
+
+
+      raise 'kek'
+
+    end
+
+
     ## utils
+
+    def ts_occurs
+      'array_length((ts).positions, 1)'
+    end
 
     def model(name, &blk)
       cap = name.capitalize
@@ -176,7 +254,8 @@ module Classification
     end
 
     def insert(table, data)
-      execute "insert into #{table.table_name} #{data.to_sql}"
+      d = data.respond_to?(:to_sql) ? data.to_sql : data
+      execute "insert into #{table.table_name} #{d}"
     end
 
 
--- lib/tasks/classify.rake
+++ lib/tasks/classify.rake
@@ -1,70 +1,11 @@
 namespace :classify do
   desc "Classify packages"
   task run: :environment do
-    data = File.read('classifier.dat')
+    k = Classification::Classifier
+    cls = k.new
 
-    STDIN.each_slice(100) do |slice|
-      c = Classify.new(Marshal.load(data))
-      slice.each do |line|
-        id, text, cats = JSON.parse(line)
-        puts [id, c.classify(text)].to_json
-      end
-    end
-  end
+    cls.prepare
 
-  desc "Print params for classification, done on backend"
-  task print_params: :environment do
-    Classify.print_params(true)
-  end
-
-  desc "Print params for classification for unclassified packages"
-  task print_params_unclassified: :environment do
-    Classify.print_params(false)
-  end
-
-  desc "Builds new model with packages from stdin"
-  task train: :environment do
-    c = Classify.new
-    STDIN.each do |line|
-      line = JSON.parse(line)
-      c.add(line[1], line[2])
-    end
-
-    STDERR.puts "Using GSL/#{GSL::VERSION} RubyGSL/#{GSL::RUBY_GSL_VERSION}"
-    STDERR.puts "Building index"
-    t1 = Time.now
-    c.build
-    t2 = Time.now
-    STDERR.puts "Index built in #{t2 - t1} seconds"
-
-    File.open('classifier.dat', 'wb') { |f| f.write(c.dump) }
-  end
-
-  desc "Sets category_scores, done on backend"
-  task update: :environment do
-    puts "UPDATE"
-    cur_package = nil
-    h = nil
-
-    STDIN.each do |line|
-      line = JSON.parse(line)
-      package = line['package']
-      if package != cur_package
-        Package.where(id: cur_package).update_all(predicted_categories: h) if cur_package
-
-        cur_package = package
-        h = {}
-      end
-
-      h.deep_merge!(
-        line['round'].to_s => {
-          line['theme'].to_s => {
-            line['question'].to_s => line['cat']
-          }
-        }
-      )
-    end
-
-    Package.where(id: cur_package).update_all(predicted_categories: h)
+    pp k::Termprob.limit(10).as_json
   end
 end
