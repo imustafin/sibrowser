117cf0cfb12701fb1d2d752a024df03546cec6c8
Ilgiz Mustafin <ilgimustafin@gmail.com>
2023-01-18 21:44:46 +0300

    Add cat only button

--- app/components/packages/show/admin/structure_classification/component_controller.js
+++ app/components/packages/show/admin/structure_classification/component_controller.js
@@ -14,9 +14,15 @@ export default class extends Controller {
   }
 
   setThemeCat({ target, params }) {
-    let { round, theme, ans, cat } = params;
+    let { round, theme, ans, cat, only } = params;
 
     let table = this.tableTarget;
+
+    if (only) {
+      table.querySelectorAll(`input[id^="${round}_${theme}_"][id$="no"][type="radio"]`)
+        .forEach(radio => radio.checked = true);
+    }
+
     let radios = table.querySelectorAll(
       `input[id^="${round}_${theme}_"][id$="${cat}_${ans}"][type="radio"]`
     );
